f802afadf8c8503888ab35c7b858688670e30533
Verlee Washington <VWashington@my365.bellevue.edu>
2020-10-16 18:37:35 -0500

    Updating

--- server/app.js
+++ server/app.js
@@ -33,7 +33,7 @@ app.use('/', express.static(path.join(__dirname, '../dist/nodebucket')));
 const port = process.env.PORT || 3000; // server port
 
 // TODO: This line will need to be replaced with your actual database connection string
-const conn = 'mongodb+srv://superadmin:s3cret@cluster0-lujih.mongodb.net/nodebucket?retryWrites=true&w=majority';
+const conn = 'mongodb+srv://superadmin:s3cret@cluster0-lujih.mongodb.net/nodebucket';
 
 /**
  * Database connection
